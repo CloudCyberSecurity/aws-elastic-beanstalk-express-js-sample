86d7a5e88e6285b6ad0e49ef5604e83ec92ab466
Imran <nationalcybersecure@gmail.com>
2021-02-25 10:46:30 +0000

    auto deploy test1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello and Welcome to my Web App pipeline test'));
+app.get('/', (req, res) => res.send('Hello and Welcome to my Web App pipeline auto deploy test1'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
